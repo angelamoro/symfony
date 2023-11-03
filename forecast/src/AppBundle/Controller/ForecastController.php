186f397c300df33864acf7a08f29e73887c5db5e
angelamoro <angela.alonso.moro@gmail.com>
2023-11-03 17:43:19 +0100

    cambios

---
+++ forecast/src/AppBundle/Controller/ForecastController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use sensio\bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForecastController extends Controller
+{
+
+    public function indexAction(){
+        return new Response ('It\'s freezing cold!');
+    }
+}
